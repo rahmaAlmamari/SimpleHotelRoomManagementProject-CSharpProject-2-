33dcb4d091a3a104480caa86f53e02a23c6bd1f4
rahmaAlmamari <alr816125@gmail.com>
2025-04-07 16:16:55 +0400

    start working in Reserve Room ForGues

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Project2
 {
@@ -10,17 +12,19 @@ namespace Project2
         static int MAX_ROOMS;
         static int roomCount = 0;
         // declare the array and variable needed ....
-        static int[] roomNumbers = new int[MAX_ROOMS];
-        static double[] roomRates = new double[MAX_ROOMS];
-        static bool[] isReserved = new bool[MAX_ROOMS];
-        static string[] guestNames = new string[MAX_ROOMS];
-        static int[] nights = new int[MAX_ROOMS];
-        static DateTime[] bookingDates = new DateTime[MAX_ROOMS];
+        static int[] roomNumbers;
+        static double[] roomRates;
+        static bool[] isReserved; // fals -> not reserved (Available) ... true -> reserved 
+        static string[] guestNames;
+        static int[] nights;
+        static DateTime[] bookingDates;
 
 
 
         static void Main(string[] args)
         {
+
+            
             bool capacity_flag = false;
             Console.WriteLine("Welcome to Hotel Room Management system ... please enter your system capacity:");
             do
@@ -33,12 +37,18 @@ namespace Project2
                 catch (Exception e)
                 {
                     Console.WriteLine("choosing option process is stoped due to: " + e.Message + "please click enter key to start over");
-                   // Console.ReadLine();//just to hold second ...
+                    // Console.ReadLine();//just to hold second ...
                     capacity_flag = true;
                 }
             } while (capacity_flag);
- 
-            
+
+            //seting the array size using MAX_ROOMS variable ...
+            roomNumbers = new int[MAX_ROOMS];
+            roomRates = new double[MAX_ROOMS];
+            isReserved = new bool[MAX_ROOMS];
+            guestNames = new string[MAX_ROOMS];
+            nights = new int[MAX_ROOMS];
+            bookingDates = new DateTime[MAX_ROOMS];
 
             // we use while loop to repeat the process and we set true so it will not stop ... 
             while (true)
@@ -104,19 +114,287 @@ namespace Project2
         //1. Add a new room ...
         static void AddNewRoom()
         {
-            Console.WriteLine("1");
+            char choice;
+            // do loop to repeat the process of adding new Room 
+            //based on the user choice ...
+            do
+            {
+                ////declare variables to holde room info ...
+                int RoomNumber = 0;
+                double roomDailyRate = 0;
+
+                
+
+                ////to make such that the user do not enter record more than
+                //// the arrays size ...
+                if (roomCount < MAX_ROOMS)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter the following room details:");
+
+                    //room number input process code ... 
+                    bool flag_rom_number = true;//to know if the room number add or not ...
+                    do
+                    {
+                        try
+                        {
+                            //to store room number into RoomNumber variable ....
+                            Console.WriteLine("Room number (Room number must be unique and > zero):");
+                            RoomNumber = int.Parse(Console.ReadLine());
+                            //to check if the room number is vailde or not (Room number must be unique) ...
+                            if (RoomNumber < 0)
+                            {
+                                Console.WriteLine("Room number not vaild it must be > zero");
+                                flag_rom_number = true;
+                            }
+                            else
+                            {
+                                flag_rom_number = false;
+                            }
+                            //to check if the room number is unique or not ...
+                            for(int i = 0; i<roomCount; i++)
+                            {
+                                if (RoomNumber == roomNumbers[i])
+                                {
+                                    Console.WriteLine("Room number not vaild it must be unique");
+                                    flag_rom_number = true;
+                                }
+                                else
+                                {
+                                    flag_rom_number = false;
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Student age not add due to: " + e.Message);
+                        }
+
+                    } while (flag_rom_number);
+
+                    //room daily rate input process code ... 
+                    bool flag_daily_rate = true;//to know if the room daily rate add or not ...
+                    do
+                    {
+                        try
+                        {
+
+                            //to store room daily rate into roomDailyRate variable ....
+                            Console.WriteLine("Room Daily Rate (Rate must be >= 100):");
+                            roomDailyRate = double.Parse(Console.ReadLine());
+                            //to check if the room daily rate is vailde or not (Rate must be >= 100) ...
+                            if (roomDailyRate < 100)
+                            {
+                                Console.WriteLine("Room daily rate not vaild");
+                            }
+                            else
+                            {
+                                flag_daily_rate = false;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Room daily rate not add due to: " + e.Message);
+                        }
+
+                    } while (flag_daily_rate);
+
+                    
+                    //to store room info in the arrays ...
+                    roomNumbers[roomCount] = RoomNumber;
+                    roomRates[roomCount] = roomDailyRate;
+                    isReserved[roomCount] = false;
+                    // so the system know that there are one more room added ......
+                    roomCount++;
+                    Console.WriteLine("Room add successfully ...");
+                    Console.WriteLine("Do you want to add anther room? y / n");
+                    choice = Console.ReadKey().KeyChar;
+                    Console.ReadLine();//just to hold second ...
+                }
+                else
+                {
+                    Console.WriteLine("Sory you can not add more Room there are no space remain!");
+                    Console.WriteLine();
+                    choice = 'n';
+                }
+
+            } while (choice == 'y' || choice == 'Y');
         }
 
         // 2. View all rooms ...
         static void ViewAllRooms()
         {
-            Console.WriteLine("2");
+            string Status;
+            Console.WriteLine("Room Information: \nNumber | Daily Rate | Status  | Status Details\n");
+            //to print all the recored based on roomCount ...
+            for (int i = 0; i < roomCount; i++)
+            {
+                if (isReserved[i] == false)
+                {
+                    Status = "Available";
+                }
+                else
+                {
+                    Status = "Reserved";
+                }
+                Console.WriteLine($"{roomNumbers[i]} | {roomRates[i]} | {Status}");
+            }
         }
 
         //3. Reserve a room for a gues ...
         static void ReserveRoomForGues()
         {
-            Console.WriteLine("3");
+            char choice;
+            // do loop to repeat the process of reseve room 
+            //based on the user choice ...
+            do
+            {
+
+                //part 1 code ... (to display all available room and tell the user if there is no)
+                int notAvailable = 0;
+                int isAvailable = 0;
+                Console.WriteLine("Room Available: \nNumber | Daily Rate\n");
+                //to print all the recored based on roomCount ...
+                for (int i = 0; i < roomCount; i++)
+                {
+                    if (isReserved[i] == false)
+                    {
+                        Console.WriteLine($"{roomNumbers[i]} | {roomRates[i]}");
+                    }
+                    else
+                    {
+                        notAvailable++;
+                    }
+
+                }
+                if (notAvailable == roomCount)
+                {
+                    Console.WriteLine("Sory there is no more room available ...");
+                }
+
+                //part 2 code ... (to allow the user to Reserve a room)
+                string guestName;
+                int nights_number = 0;
+                int roomNumToReseve = 0;
+                DateTime guest_date;
+                int index = 0;
+                //to make sure that reserve room process can be repeat based on the room number availble ...
+                int tryToReseveRoom = 0;
+                if (roomCount > tryToReseveRoom)
+                {
+                    //Console.Clear();
+                    Console.WriteLine("Enter the following details:");
+
+                    //guest name input process code ... 
+                    bool flag_name = false;//to know if the name add or not ...
+                    do
+                    {
+                        //to store guest name into guestName variable ....
+                        Console.WriteLine("Guest Name:");
+                        
+                        guestName = Console.ReadLine();
+                        bool check_name = IsAlpha(guestName);
+                        if (check_name == false)
+                        {
+                            Console.WriteLine("Guest name can not contains number and con not be null ..." +
+                                              "please enter student name again");
+                            flag_name = true;
+                        }
+                        else
+                        {
+                            flag_name = false;
+                        }
+
+                    } while (flag_name);
+
+                    //room number input process code ... 
+                    bool flag_rom_number = false;//to know if the room number is there or not ...
+                    do
+                    {
+                        try
+                        {
+                            //to store room number into RoomNumber variable ....
+                            Console.WriteLine("Enter room number you want to reseved:");
+                            roomNumToReseve = int.Parse(Console.ReadLine());
+                            //to check if the room number is there or not ...
+                            for (int i = 0; i < roomCount; i++)
+                            {
+                                if (roomNumToReseve == roomNumbers[i])
+                                {
+                                    if (isReserved[i] == false)
+                                    {
+                                        //isReserved[i] = true;
+                                        index = i;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("This room is alraedy reseved");
+                                        flag_rom_number = true;
+                                    }
+                                    
+                                }
+                                else
+                                {
+                                    Console.WriteLine("This room number do not exit please enter anther number!");
+                                    flag_rom_number = true;
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Room number not entered due to: " + e.Message);
+                        }
+
+                    } while (flag_rom_number);
+
+                    //nights number input process code ... 
+                    bool flag_nights_number = false;//to know if the nights number is added or not ...
+                    do
+                    {
+                        try
+                        {
+                            //to store nights number into nights_number variable ....
+                            Console.WriteLine("Enter nights number you want to reseved:");
+                            nights_number = int.Parse(Console.ReadLine());
+                            //to check if the nights number is > zero or not ...
+                            if (nights_number < 0)
+                            {
+                                Console.WriteLine("Nights number should be > zero");
+                                flag_nights_number = true;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Nights number not entered due to: " + e.Message);
+                        }
+
+                    } while (flag_nights_number);
+
+
+                    //to store date of taday into guest_date varible ....
+                    guest_date = DateTime.Now;
+
+                    //to store guest info in the arrays ...
+                    guestNames[tryToReseveRoom] = guestName;
+                    nights[tryToReseveRoom] = nights_number;
+                    bookingDates[tryToReseveRoom] = guest_date;
+                    isReserved[index] = true;
+
+                    // so the system know that there are one more room reseved ......
+                    tryToReseveRoom++;
+                    Console.WriteLine("Room reseve successfully ...");
+                    Console.WriteLine("Do you want to reseve anther room? y / n");
+                    choice = Console.ReadKey().KeyChar;
+                    Console.ReadLine();//just to hold second ...
+                }
+                else
+                {
+                    Console.WriteLine("Sory you can not reseve more room there are no room remain!");
+                    Console.WriteLine();
+                    choice = 'n';
+                }
+
+            } while (choice == 'y' || choice == 'Y');
         }
 
         //4. View all reservations with total cost ...
@@ -142,5 +420,15 @@ namespace Project2
         {
             Console.WriteLine("7");
         }
+
+
+        //ADDITIONAL METHODS ...
+        //1. To check of the string contains something other than letters (this methos return true or false)....
+        static bool IsAlpha(string input)
+        {
+            return Regex.IsMatch(input, "^[a-zA-Z]+$");
+        }
+
+
     }
 }
